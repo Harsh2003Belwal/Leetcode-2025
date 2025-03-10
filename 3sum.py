cdd5285d927dddad4cd9a3673d4c971d5d50ab36
Harsh Belwal <110084749+Harsh2003Belwal@users.noreply.github.com>
2025-03-10 08:34:02 -0700

    3sum.py

--- 3sum.py
+++ 3sum.py
@@ -1,9 +1,9 @@
 def threeSum(nums):
         st = []
         n=len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                for k in range(j + 1, n):
+        for i in range(n-1):
+            for j in range(i + 1, n-1):
+                for k in range(j + 1, n-1):
                     if nums[i] + nums[j] + nums[k] == 0:
                         temp = [nums[i], nums[j], nums[k]]
                         temp.sort()
